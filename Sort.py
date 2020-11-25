458c6e4fbf8b16b5cbb84c6b6b1651c9a99527d5
udaysk.mondal@gmail.com <udaysk.mondal@gmail.com>
2020-11-25 21:35:31 +0600

    List_sort

--- Sort.py
+++ Sort.py
@@ -2,3 +2,12 @@ names = ['Xander', 'Buffy', 'Angel', 'Willow', 'Giles']
 names.sort()
 
 print(names)
+print("gitPushed")
+
+# Another way to Sort List ----Using sorted() method----
+
+names = ['Xander', 'Buffy', 'Angel', 'Willow', 'Giles']
+names_sorted = sorted(names)
+
+print(names_sorted)
+
