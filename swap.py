ab7a086923e086131c9bf15da39da90d0fc8038d
udaysk.mondal@gmail.com <udaysk.mondal@gmail.com>
2020-11-27 20:00:02 +0600

    swap-BubbleSort-UsingList

--- swap.py
+++ swap.py
@@ -4,10 +4,10 @@ def swap(arr, pos_1, pos_2):
   arr[pos_2] = tmp
   return arr
 
-arrList = [19,65,25,20]
+arrList = [19,65, 88, 353, 225, 332]
 
 pos11 = 0
-pos22 = 1
+pos22 = 2
 
 print(swap(arrList,pos11,pos22))
 
