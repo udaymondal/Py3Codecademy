919cf2ad585d58fc9351340afa45c045f586185b
udaysk.mondal@gmail.com <udaysk.mondal@gmail.com>
2020-11-27 20:25:43 +0600

    swap_DS

--- swap.py
+++ swap.py
@@ -5,9 +5,12 @@ def swap(arr, pos_1, pos_2):
   return arr
 
 arrList = [19,65, 88, 353, 225, 332]
+print("Original" , arrList)
+arrList.append(22)
+arrList.remove(225)
 
 pos11 = 0
-pos22 = 2
-
+pos22 = 1
+print("\nAfter operations")
 print(swap(arrList,pos11,pos22))
 
